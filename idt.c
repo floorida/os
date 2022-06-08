9ee66825990efd965b80627da05da7cb704cc7ef
floorida <legoboy881@gmail.com>
2022-06-08 20:38:54 +0300

    commit

--- idt.c
+++ idt.c
@@ -20,5 +20,11 @@ void idt_init()
 {
     idtr.base = (uintptr_t)&idt[0];
     idtr.limit = (uint16_t)sizeof(idt_desc_t) * 256 - 1;
+
+    for (uint8_t vector = 0; vector < 255; vector++)
+    {
+        idt_set_descriptor(vector, isr_stub_table[vector], 0x8E);
+    }
+
     __asm__("lidt %0" ::"m"(idtr));
 };
